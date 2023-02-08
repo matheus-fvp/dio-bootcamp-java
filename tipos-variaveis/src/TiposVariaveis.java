ff0d5cbf3be8f4eb8d6872f1abfc22df99d84dfb
Matheus Fernando Vieira Pinto <vieira.matheusf13@gmail.com>
2023-02-08 20:43:38 -0300

    tipos primitivos, constantes, strings

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,24 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        //tipos primitivos
+        char caractere = 'a';
+        byte numero = 121;
+        short numeroCurto = 1001;
+        int numeroNormal = 10989;
+        long numeroLongo = 100002222L;
+        //numero = numeroCurto //erro
+        
+        double salarioMinimo = 2500.00;
+        float pi = 3.14F;
+
+        boolean logico = true;
+
+        //Constantes
+        final float VALOR_DE_PI = 3.14F;
+
+        //Strings
+        String meuNome = "matheus";
+
     }
 }

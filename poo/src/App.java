6991c465ebb2d87e93d6dff7cbaa4a565fb9582f
Matheus Fernando Vieira Pinto <vieira.matheusf13@gmail.com>
2023-02-11 13:39:55 -0300

    objetos e mensagem

--- poo/src/App.java
+++ poo/src/App.java
@@ -1,5 +1,11 @@
+import model.Carro;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Carro bmw = new Carro("cinza", "bmw", 100.00);
+        Carro fiatUno = new Carro("branco", "uno", 50.00);
+
+        System.out.println(fiatUno.calcularValorTanqueCompleto(4.85));
+        System.out.println(bmw.calcularValorTanqueCompleto(4.85));
     }
 }
